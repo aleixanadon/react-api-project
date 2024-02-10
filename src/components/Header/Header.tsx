c2e5ff169a059a368b388e71036bc8d05c081f6a
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2024-02-10 02:16:01 +0100

    starting to develop header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>🐱 ADOPT A MEOW</h1>
+      <nav>
+        <ul>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
