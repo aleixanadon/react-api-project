fa7350f2d4e9697a3361008c740d3e8d1be1a967
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2024-02-12 02:58:24 +0100

    changed project structure for the future react router dom

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./components/HeroSection/HeroSection"
+
+const HomePage = () => {
+  return (
+    <main className="main-home-page">
+      <HeroSection />
+    </main>
+  )
+}
+
+export default HomePage
---
+++ src/pages/Home/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className='hero-section'>
+      <article>
+        <h2>ADOPT YOUR NEW FRIEND MEOW!</h2>
+        <h3>Everyday's a purrfect day</h3>
+        <h4>Adopt your first cat today</h4>
+        <div>
+          <button>ADOPT NOW</button>
+          <button>READ OUR TERMS</button>
+        </div>
+      </article>
+      <article>
+        <img src="" alt="" />
+      </article>
+    </section>
+  )
+}
+
+export default HeroSection
