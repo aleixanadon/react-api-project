a7309bb47de8462b3c8076ac108f6bd3d202b089
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2024-02-12 16:31:51 +0100

    removed unused import from app.tsx to make deploy work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './common/components/Header/Header'
-import HomePage from './pages/Home/HomePage'
 
 function App() {
   return (
