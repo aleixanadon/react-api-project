7456f5d218bd8a349891eb6b9d57de994507c6b9
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2024-02-20 17:42:02 +0100

    reseted app.css styles to design header in mobile view, styled header and dropdown buttons to make the navbar appear when you click de button

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom'
 import './header.css'
+import { useState } from 'react'
 
 const Header = () => {
+
+  const [isNavbarOpen, setNavbarVisibility] = useState(true);
+  const toggleNavbar = () => setNavbarVisibility(!isNavbarOpen);
+
   return (
     <header>
       <h1>
-        <Link to="/react-api-project/">🐱 ADOPT A MEOW</Link>
+        <Link to="/react-api-project/" className='title-container'>
+          <div className='icon'>🐱</div>
+          <div className='app-title'>ADOPT A MEOW</div>
+        </Link>
       </h1>
-      <nav>
+      <nav className={isNavbarOpen ? 'navbar-open' : ''}>
         <ul>
           <li>
             <Link to="/react-api-project/pricing">PRICING</Link>
@@ -23,6 +31,12 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+      <button 
+        className='dropdown-button'
+        onClick={toggleNavbar}
+      >
+        ☰
+      </button>
     </header>
   )
 }
