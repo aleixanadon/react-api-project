ad2d849d9eb89ba4a9f590c76d367b43266d35a6
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2024-02-27 13:00:25 +0100

    trying to fix structure and styles from pc view with a design made with figma

--- src/pages/Home/components/HeroSection/HeroSection.tsx
+++ src/pages/Home/components/HeroSection/HeroSection.tsx
@@ -5,15 +5,24 @@ import cat from '../../../../assets/cat.png'
 const HeroSection = () => {
   return (
     <section className='hero-section'>
-      <article>
-        <div>
-          <h2 className='h2-title'>ADOPT MEOW!</h2>
-          <p className='h2-subtitle'>Everyday's a purrfect day to adopt your new kitty friend!</p>
-        </div>
-        <div className='adopt-now-div'>
-          <Link to={""} className="">
-            ADOPT NOW
-          </Link>
+      <article className='hero-section-article'>
+        <div className='hero-section-div'>
+          <div>
+            <h2 className='h2-title'>ADOPT MEOW!</h2>
+            <p className='h2-subtitle'>Everyday's a purrfect day to adopt your new kitty friend!</p>
+          </div>
+          <div>
+            <div className='adopt-now-div'>
+              <Link to={""} className="">
+                I WANT TO ADOPT
+              </Link>
+            </div>
+            <div className='adopt-now-div'>
+              <Link to={""} className="">
+                READ OUR TERMS
+              </Link>
+            </div>
+          </div>
         </div>
         <img src={cat} alt="" className='cat-photo' />
       </article>
