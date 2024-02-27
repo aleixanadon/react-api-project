283b98d90be7491c565485e2ab193ce75f6ba764
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2024-02-27 20:02:55 +0100

    available cats section designed on mobile phone

--- src/pages/Home/components/AvailableCatsSection/AvailableCatsSection.tsx
+++ src/pages/Home/components/AvailableCatsSection/AvailableCatsSection.tsx
@@ -6,8 +6,12 @@ import './availableCatsSection.css'
 const AvailableCatsSection = () => {
   return (
     <section>
-      <h2 className='available-cats'>AVAILABLE CATS</h2> 
-      <article>
+      <h2 className='available-cats-h2'>AVAILABLE CATS</h2> 
+      <article className='list-of-cats'>
+        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
+        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
+        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
+        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
         <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
       </article>
       <Link to="">See more</Link>
