169e0baa189093b32b7f42c7d2e6acc545f8b70c
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2024-03-01 11:32:20 +0100

    styles changed but it needs more improvement

--- src/pages/Home/components/AvailableCatsSection/AvailableCatsSection.tsx
+++ src/pages/Home/components/AvailableCatsSection/AvailableCatsSection.tsx
@@ -2,22 +2,33 @@ import { Link } from 'react-router-dom'
 import CatCard from '../../../../common/components/CatCard/CatCard'
 import catPhoto from '../../../../assets/cat.png'
 import './availableCatsSection.css'
+import useCatApi from '../../../../common/hooks/useCatApi'
+import { useEffect } from 'react'
 
 const AvailableCatsSection = () => {
+  const { catApiData, fetchData, loading } = useCatApi();
+
+  useEffect(() => {
+    fetchData(16)
+  }, []);
+
   return (
     <section>
       <h2 className='available-cats-h2'>AVAILABLE CATS</h2> 
       <article className='list-of-cats'>
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
-        <CatCard src={catPhoto} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
+        {
+          loading ? (
+            <>loading</>
+          ) : (
+            <>
+              {
+                catApiData.map((value, _index) => (
+                  <CatCard src={value.url} description='Encantador gato egipcio, juguetón y curioso, tiene 4 meses de edad.' />
+                ))
+              }
+            </>
+          )
+        }
       </article>
       <Link to="">See more</Link>
     </section>
